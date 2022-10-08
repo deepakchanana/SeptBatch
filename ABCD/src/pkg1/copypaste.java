41029f6251e56d15437a47295edfeed12a015446
deepakchanana <deepakdeep1992@gmail.com>
2022-10-08 20:17:58 +0530

    pushing code first time

---
+++ ABCD/src/pkg1/copypaste.java
@@ -0,0 +1,34 @@
+package pkg1;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class copypaste
+{
+	public void ReadWrite() throws IOException
+	{
+		File f1=new File("../ABCD/inputfile.txt");
+		File f2=new File("../ABCD/inputfile2.txt");
+		FileReader fr=new FileReader(f1);
+		FileWriter fw=new FileWriter(f2);
+		BufferedReader br=new BufferedReader(fr);
+		BufferedWriter bw=new BufferedWriter(fw);
+		String s;
+		while((s=br.readLine())!=null)
+		{
+			bw.write(s);
+			bw.newLine();
+		}
+		bw.close();
+	}
+	public static void main(String[] args) throws IOException 
+	{
+		copypaste c=new copypaste();
+		c.ReadWrite();
+	}
+}
